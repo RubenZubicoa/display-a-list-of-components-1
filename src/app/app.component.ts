c56e8224e1810c87b84342ed1a452611f209fa23
Ruben Zubicoa <ruben.zubicoa.garijo@ibm.com>
2024-09-03 16:08:33 +0200

    FEAT: complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit, Signal } from '@angular/core';
 import {MovieItemComponent} from './movie-item/movie-item.component';
 import {Movie} from './model/movie.model';
 import { MoviesService } from './services/movies.service';
@@ -7,15 +7,17 @@ import { MoviesService } from './services/movies.service';
 @Component({
   selector: 'app-root',
   standalone: true,
-  
+  providers: [MoviesService],
   templateUrl: 'app.component.html',
   imports: [
     MovieItemComponent
   ],
 })
-export class AppComponent {
+export class AppComponent implements OnInit  {
 
   private moviesService = inject(MoviesService)
+
+  public movies: Signal<Movie[]>;
 
   movie: Movie =  {
     "id": "e80d5a37-620e-4be2-92b9-fb1f5262494f",
@@ -24,4 +26,8 @@ export class AppComponent {
     "budget": 125,
     "release_date": "2001-11-04"
   };
+
+  ngOnInit(): void {
+      this.movies = this.moviesService.getMovies();
+  }
 }
